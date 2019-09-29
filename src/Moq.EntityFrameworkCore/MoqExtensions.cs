9628d885fd81f244e95335cfe898119670aa9df6
Michał Jankowski <jankowski.m@gmail.com>
2019-09-29 10:13:43 +0200

    Fix issue connected to using LINQ multiple times

--- src/Moq.EntityFrameworkCore/MoqExtensions.cs
+++ src/Moq.EntityFrameworkCore/MoqExtensions.cs
@@ -46,7 +46,7 @@
 
             dbSetMock.As<IQueryable<TEntity>>().Setup(m => m.Expression).Returns(entitiesAsQueryable.Expression);
             dbSetMock.As<IQueryable<TEntity>>().Setup(m => m.ElementType).Returns(entitiesAsQueryable.ElementType);
-            dbSetMock.As<IQueryable<TEntity>>().Setup(m => m.GetEnumerator()).Returns(entitiesAsQueryable.GetEnumerator());
+            dbSetMock.As<IQueryable<TEntity>>().Setup(m => m.GetEnumerator()).Returns(() => entitiesAsQueryable.GetEnumerator());
         }
     }
 }
